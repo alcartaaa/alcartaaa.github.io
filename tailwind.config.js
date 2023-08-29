dbad1603182479929b4bbdd1b8986f630f95e9f0
alcartaaa <alkahfichandra26@gmail.com>
2023-08-29 19:12:17 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./index.html'],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
+  module.exports = {
+    theme: {
+      fontFamily: {
+        'sans': ['ui-sans-serif', 'system-ui'],
+        'body': ['Poppins', 'sans-serif'],
+      }
+    }
+  }
